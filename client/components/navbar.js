8e9a43a4046d4ce7057760cf2350e50cb18bd558
Maggie Walker <mags21walker@gmail.com>
2019-03-02 17:55:00 -0500

    Update Lodash version

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,7 +3,6 @@ import React from 'react'
 const Navbar = () => (
   <div>
     <img src="./BetweenBarsSimple.png" />
-    <a href="https://github.com/MaggieWalker/RapBoardReact"><img src="https://image.flaticon.com/icons/png/512/25/25231.png"/></a>
   </div>
 )
 
